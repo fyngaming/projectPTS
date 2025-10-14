c3219ff155f254fc9df9f52dc9dd7099b44ffb08
fyngaming <favianyusuf9@gmail.com>
2025-10-14 19:15:23 +0700

    tambahan activity 4

--- app/src/main/java/com/example/projectpts/MainActivity.java
+++ app/src/main/java/com/example/projectpts/MainActivity.java
@@ -1,17 +1,20 @@
 package com.example.projectpts;
 
 import androidx.appcompat.app.AppCompatActivity;
+
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.LinearLayout;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
     EditText etUsername, etPassword;
     Button btnLogin;
+    LinearLayout btnLoginGoogle;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -21,6 +24,7 @@ public class MainActivity extends AppCompatActivity {
         etUsername = findViewById(R.id.etUsername);
         etPassword = findViewById(R.id.etPassword);
         btnLogin = findViewById(R.id.btnLogin);
+        btnLoginGoogle = findViewById(R.id.btnLoginGoogle);
 
         btnLogin.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -38,6 +42,18 @@ public class MainActivity extends AppCompatActivity {
                 } else {
                     Toast.makeText(MainActivity.this, "Username / Password salah!", Toast.LENGTH_SHORT).show();
                 }
+            }
+        });
+
+        // Button Google Login
+        btnLoginGoogle.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(MainActivity.this, "Login dengan Google", Toast.LENGTH_SHORT).show();
+
+                Intent intent = new Intent(MainActivity.this, MainActivity2.class);
+                startActivity(intent);
+                finish();
             }
         });
     }
